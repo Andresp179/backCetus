1fd251cea0ac3009b91c08b138ff716967f8e79c
Andresp179 <perezandres1799@gmail.com>
2024-03-24 16:22:41 -0500

    Se adelanta 2 enpoint mas de producto

--- src/main/java/com/example/demo/Controller/ProductoController.java
+++ src/main/java/com/example/demo/Controller/ProductoController.java
@@ -3,11 +3,15 @@ package com.example.demo.Controller;
 
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Models.Producto;
 import com.example.demo.Repository.ProductoDao;
+import com.gestion.empleados.excepciones.ResourceNotFoundException;
+import com.gestion.empleados.modelo.Empleado;
+
 
 
 @RestController
@@ -31,5 +38,17 @@ public class ProductoController {
 	public List<Producto> listarProductos() {
 		return productoServiceImpl.findAll();
 	}
+	
+	//este metodo sirve para guardar el PRODUCTO
+		@PostMapping("/api/v1/productos")
+		public Producto guardarProducto(@RequestBody Producto producto) {
+			return productoServiceImpl.save(producto);}
+		
+		//este metodo sirve para buscar un empleado
+		@GetMapping("/empleados/{id}")
+		public ResponseEntity<Optional<Producto>> obtenerProductoPorId(@PathVariable Long id){
+				Optional<Producto> producto = productoServiceImpl.findById(id);						            
+				return ResponseEntity.ok(producto);
+		}
 	
 }
