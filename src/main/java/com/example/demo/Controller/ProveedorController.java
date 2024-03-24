46fd30e9efb9b93ac8bb283dc84860c02afca586
Andresp179 <perezandres1799@gmail.com>
2024-03-24 08:19:36 -0500

    Controller proveedor

--- src/main/java/com/example/demo/Controller/ProveedorController.java
+++ src/main/java/com/example/demo/Controller/ProveedorController.java
@@ -1,11 +1,30 @@
 package com.example.demo.Controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Models.Proveedor;
+import com.example.demo.Repository.ProveedorDao;
 
+@Controller
+@RequestMapping("/")
+@CrossOrigin(origins="http://localhost:4200/")
 @RestController
 public class ProveedorController {
 	
+	@Autowired
+	public ProveedorDao proveedorDao;
+	
+	@RequestMapping(value = "/api/v1/proveedores", method = RequestMethod.GET)
+	public List<Proveedor> listarProveedores(){		
+		return proveedorDao.findAll();
+	}
 	
 }
