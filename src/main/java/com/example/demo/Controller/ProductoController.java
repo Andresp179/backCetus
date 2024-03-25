e62cf526eae24ac28e259f8b5d48d050005fb354
Andresp179 <perezandres1799@gmail.com>
2024-03-25 08:50:55 -0500

    Se ajusta controller Producto y excepciones

--- src/main/java/com/example/demo/Controller/ProductoController.java
+++ src/main/java/com/example/demo/Controller/ProductoController.java
@@ -1,53 +1,83 @@
 package com.example.demo.Controller;
 
-
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Excepciones.ResourceNotFoundException;
 import com.example.demo.Models.Producto;
 import com.example.demo.Repository.ProductoDao;
 
 
 
-
 @RestController
 @RequestMapping
-@CrossOrigin(origins="http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class ProductoController {
 
 	@Autowired
 	private ProductoDao productoServiceImpl;
 
-		
 	@RequestMapping(value = "/api/v1/productos", method = RequestMethod.GET)
 	public List<Producto> listarProductos() {
 		return productoServiceImpl.findAll();
 	}
+
+	// este metodo sirve para guardar el PRODUCTO
+	@PostMapping("/api/v1/productos")
+	public Producto guardarProducto(@RequestBody Producto producto) {
+		return productoServiceImpl.save(producto);
+	}
+
+	// este metodo sirve para buscar un empleado
+	@GetMapping("/productos/{id}")
+	public ResponseEntity<Producto> obtenerEmpleadoPorId(@PathVariable Long id) {
+		Producto producto = productoServiceImpl.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No existe el producto con el ID : " + id));
+		return ResponseEntity.ok(producto);
+	}
 	
-	//este metodo sirve para guardar el PRODUCTO
-		@PostMapping("/api/v1/productos")
-		public Producto guardarProducto(@RequestBody Producto producto) {
-			return productoServiceImpl.save(producto);}
+	//este metodo sirve para actualizar empleado
+		@PutMapping("/productos/{id}")
+		public ResponseEntity<Producto> actualizarEmpleado(@PathVariable Long id,@RequestBody Producto detallesProducto){
+			Producto producto = productoServiceImpl.findById(id)
+					            .orElseThrow(() -> new ResourceNotFoundException("No existe el producto con el ID : " + id));
+			
+			producto.setNombreProducto(detallesProducto.getNombreProducto());
+			producto.setDescripcion(detallesProducto.getDescripcion());
+			producto.setEstado(detallesProducto.getEstado());
+			producto.setNombreLaboratorio(detallesProducto.getNombreLaboratorio());
+			
+			Producto productoActualizado = productoServiceImpl.save(producto);
+			return ResponseEntity.ok(productoActualizado);
+	    }
 		
-		//este metodo sirve para buscar un empleado
-		@GetMapping("/empleados/{id}")
-		public ResponseEntity<Optional<Producto>> obtenerProductoPorId(@PathVariable Long id){
-				Optional<Producto> producto = productoServiceImpl.findById(id);						            
-				return ResponseEntity.ok(producto);
-		}
-	
+		@DeleteMapping("/empleados/{id}")
+		public ResponseEntity<Map<String,Boolean>> eliminarEmpleado(@PathVariable Long id){
+			Producto producto = productoServiceImpl.findById(id)
+					            .orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el ID : " + id));
+			
+			productoServiceImpl.delete(producto);
+			Map<String, Boolean> respuesta = new HashMap<>();
+			respuesta.put("eliminar",Boolean.TRUE);
+			return ResponseEntity.ok(respuesta);
+	    }
+
 }
